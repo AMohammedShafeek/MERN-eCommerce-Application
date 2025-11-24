547570109cf51414a5f690514aac519e18560262
AMohammedShafeek <amdshafeek7@gmail.com>
2025-11-24 23:56:59 +0530

    FEAT : Add Admin/Dashboard Part-1

--- admin/src/Pages/Dashboard/Dashboard.jsx
+++ admin/src/Pages/Dashboard/Dashboard.jsx
@@ -2,18 +2,38 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Divider from "@mui/material/Divider";
+import DashboardBox from "../../components/DashboardBox/DashboardBox";
 
 const Dashboard = () => {
   return (
     <section className="border-t border-t-gray-300">
-        <div className="container flex">
-          <div className="sidebarWrapper h-full w-[20%] bg-white">
-            <Sidebar></Sidebar>
-          </div>
-          <div className="sidebarWrapper w-[80%] my-5 h-full">
-            Admin
-          </div>
+      <div className="container flex">
+        <div className="sidebarWrapper h-full w-[20%] bg-white">
+          <Sidebar></Sidebar>
         </div>
+        <div className="sidebarWrapper w-[80%] my-7 h-full">
+          <div className="bg-white flex items-center gap-7 mb-3 py-3 px-4">
+            <div className="w-[30%]">
+              <img
+                src="./src/assets/svg/dashSVG.png"
+                alt=""
+                className="w-[250px]"
+              />
+            </div>
+            <div className="info leading-none w-[70%]">
+              <h1 className="text-[18px] font-bold pt-3">Good Morning,</h1>
+              <h1 className="text-[24px] primary font-bold mb-3 my-1.5 border-b pb-5 border-gray-300">
+                Mohammed Shafeek. A
+              </h1>
+              <p className="text-[14px] text-gray-500 py-2 font-[500]">
+                Here's What Happening on your Store today. See the Statistics at
+                Once.
+              </p>
+            </div>
+          </div>
+          <DashboardBox></DashboardBox>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ admin/src/components/DashboardBox/DashboardBox.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
+import "./DashboardBox.css";
+import { IoBag } from "react-icons/io5";
+import { RiMoneyRupeeCircleFill } from "react-icons/ri";
+import { BsBank2 } from "react-icons/bs";
+import Divider from "@mui/material/Divider";
+
+const DashboardBox = () => {
+  return (
+    <div className=" overflow-hidden ">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={20}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="dashSlider"
+      >
+        <SwiperSlide>
+          <div className="group flex flex-col w-full p-3 px-5 shadow shadow-lg rounded-md transition-all duration-300 bg-white">
+            <div className="flex items-center pb-3 mb-3 justify-between border-b border-gray-300">
+              <div>
+                <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                  New Orders
+                </h3>
+                <h3 className="text-[25px] primary font-bold transition-all duration-300">
+                  426
+                </h3>
+              </div>
+              <div>
+                <IoBag className="text-[50px] transition-all duration-300 group-hover:text-[#ff5252] group-hover:scale-110"></IoBag>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                <span className="text-green-600 mr-1.5">+32.40%</span>Increased
+                Last Month
+              </h3>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="group flex flex-col w-full p-3 px-5 shadow shadow-lg rounded-md transition-all duration-300 bg-white">
+            <div className="flex items-center pb-3 mb-3 justify-between border-b border-gray-300">
+              <div>
+                <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                  Sales
+                </h3>
+                <h3 className="text-[25px] primary font-bold transition-all duration-300">
+                  ₹3,50,425
+                </h3>
+              </div>
+              <div>
+                <RiMoneyRupeeCircleFill className="text-[50px] transition-all duration-300 group-hover:text-[#ff5252] group-hover:scale-110"></RiMoneyRupeeCircleFill>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                <span className="text-green-600 mr-1.5">+32.40%</span>Increased
+                Last Month
+              </h3>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="group flex flex-col w-full p-3 px-5 shadow shadow-lg rounded-md transition-all duration-300 bg-white">
+            <div className="flex items-center pb-3 mb-3 justify-between border-b border-gray-300">
+              <div>
+                <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                  Revenue
+                </h3>
+                <h3 className="text-[25px] primary font-bold transition-all duration-300">
+                  ₹70,542
+                </h3>
+              </div>
+              <div>
+                <BsBank2 className="text-[50px] transition-all duration-300 group-hover:text-[#ff5252] group-hover:scale-110"></BsBank2>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-[14px] text-black font-bold transition-all duration-300">
+                <span className="text-green-600 mr-1.5">+32.40%</span>Increased
+                Last Month
+              </h3>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default DashboardBox;
