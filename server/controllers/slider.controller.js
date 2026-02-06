b67b40bc14de0b534a956f080b1db57321f8032d
AMohammedShafeek <amdshafeek7@gmail.com>
2026-02-06 18:24:02 +0530

    FEAT: create uploadSliderImage,removeSliderImageFromCloudinary,deleteSlider,updateSlider,deleteMultipleSliders API

--- server/controllers/slider.controller.js
+++ server/controllers/slider.controller.js
@@ -127,3 +127,187 @@ export async function getSlider(request, response) {
     });
   }
 }
+
+export async function uploadImage(request, response) {
+  try {
+    imagesArr = [];
+
+    const image = request.files;
+
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: false,
+    };
+
+    for (let i = 0; i < image?.length; i++) {
+      const img = await cloudinary.uploader.upload(
+        image[i].path,
+        options,
+        function (error, result) {
+          imagesArr.push(result.secure_url);
+          fs.unlinkSync(`uploads/${request.files[i].filename}`);
+        },
+      );
+    }
+
+    return response.status(200).json({
+      error: false,
+      success: true,
+      images: imagesArr,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      error: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function removeImageFromCloudinary(request, response) {
+  try {
+    const { imageUrl } = request.query;
+
+    if (!imageUrl) {
+      return response.status(400).json({
+        error: true,
+        success: false,
+        message: "Image is Required",
+      });
+    }
+    const urlArr = imageUrl.split("/");
+    const image = urlArr[urlArr.length - 1];
+    const imageName = image.split(".")[0];
+
+    const result = await cloudinary.uploader.destroy(imageName);
+
+    return response.status(200).json({
+      error: false,
+      success: true,
+      message: "Image Deleted Successfully.",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      error: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function deleteSlider(request, response) {
+  try {
+    const { id } = request.params;
+
+    const slider = await SliderModel.findById(id).populate("category");
+
+    if (!slider) {
+      return response.status(400).json({
+        error: true,
+        success: false,
+        message: "Slider Not Found",
+      });
+    }
+
+    const urlArr = slider.image.split("/");
+    const image = urlArr[urlArr.length - 1];
+    const imageName = image.split(".")[0];
+
+    if (imageName) {
+      cloudinary.uploader.destroy(imageName, function (error, result) {});
+    }
+
+    const deletedSlider = await SliderModel.findByIdAndDelete(id);
+
+    if (!deletedSlider) {
+      return response.status(400).json({
+        error: true,
+        success: false,
+        message: "Slider Not Deleted",
+      });
+    }
+
+    return response.status(200).json({
+      error: false,
+      success: true,
+      message: "Slider Deleted Successfully",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      error: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function updateSlider(request, response) {
+  try {
+    const { id } = request.params;
+    const { name, image, category } = request.body;
+
+    const slider = await SliderModel.findByIdAndUpdate(id, {
+      name: name,
+      image: image,
+      category: category,
+    });
+
+    if (!slider) {
+      return response.status(400).json({
+        error: true,
+        success: false,
+      });
+    }
+
+    return response.status(200).json({
+      error: false,
+      success: true,
+      message: "Slider Updated Successfully",
+    });
+  } catch (error) {
+    return response.status(500).json({
+      error: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function deleteMultipleData(request, response) {
+  const { ids } = request.body;
+
+  if (!ids || !Array.isArray(ids)) {
+    return response.status(400).json({
+      error: true,
+      success: false,
+      message: "Slider IDs Are Missing",
+    });
+  }
+
+  for (let i = 0; i < ids.length; i++) {
+    const slider = await SliderModel.findById(ids[i]);
+    const imageUrl = slider.image;
+
+    const urlArr = imageUrl.split("/");
+    const image = urlArr[urlArr.length - 1];
+    const imageName = image.split(".")[0];
+
+    const result = await cloudinary.uploader.destroy(imageName);
+  }
+  try {
+    await SliderModel.deleteMany({ _id: { $in: ids } });
+
+    return response.status(200).json({
+      error: false,
+      success: true,
+      message: "Sliders Deleted Successfully.",
+    });
+  } catch (error) {
+    response.status(500).json({
+      error: true,
+      success: false,
+      message: error.message || error,
+    });
+  }
+}
--- server/route/slider.route.js
+++ server/route/slider.route.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import auth from "../middlewares/auth.js";
+import upload from "../middlewares/multer.js";
 
 //import All Controllers
 import {
   createSlider,
+  deleteMultipleData,
+  deleteSlider,
   getAllSliders,
   getSlider,
+  removeImageFromCloudinary,
+  updateSlider,
+  uploadImage,
 } from "../controllers/slider.controller.js";
 
 const sliderRouter = Router();
@@ -12,5 +19,10 @@ const sliderRouter = Router();
 sliderRouter.post("/create", createSlider);
 sliderRouter.get("/getAllSliders", getAllSliders);
 sliderRouter.get("/:id", getSlider);
+sliderRouter.post("/uploadImages", auth, upload.array("images"), uploadImage);
+sliderRouter.delete("/deleteImage", auth, removeImageFromCloudinary);
+sliderRouter.delete("delete-Multiple", deleteMultipleData);
+sliderRouter.delete("/:id", deleteSlider);
+sliderRouter.put("/updateSlider/:id", auth, updateSlider);
 
 export default sliderRouter;
